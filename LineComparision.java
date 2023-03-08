f0ec55e1ad4f836e547e8200dfa90d06a34ebc88
anuroopkanna <anuroopindia16@gmail.com>
2023-03-08 09:24:10 +0530

    added:CheckEqual

--- LineComparision.java
+++ LineComparision.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class LineComparision {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        //line 1
         System.out.println("Welcome to the Line Comparison Program. ");
         System.out.println("Enter the value of X1: ");
         int x1 =sc.nextInt();
@@ -18,7 +19,28 @@ public class LineComparision {
         int y2 = sc.nextInt();
         System.out.println("Enter the value of Y3: ");
         int y3 = sc.nextInt();
-        double length =Math.sqrt((x3-x2-x1)^2+(y3-y2-y1)^2);
-        System.out.println("Length of the line is: "+"("+x1+","+y1+"),"+"("+x2+","+y2+"),"+"("+x3+","+y3+")==>"+length);
+        double lengthofline1 =Math.sqrt((x3-x2-x1)^2+(y3-y2-y1)^2);
+        System.out.println("Length of the line1 is:" +lengthofline1);
+        //line2
+        System.out.println("Enter the value of X4: ");
+        int x4 =sc.nextInt();
+        System.out.println("Enter the value of X5: ");
+        int x5 = sc.nextInt();
+        System.out.println("Enter the value of X6: ");
+        int x6 = sc.nextInt();
+        System.out.println("Enter the value of Y4: ");
+        int y4 = sc.nextInt();
+        System.out.println("Enter the value of Y5: ");
+        int y5 = sc.nextInt();
+        System.out.println("Enter the value of Y6: ");
+        int y6 = sc.nextInt();
+        double lengthofline2 =Math.sqrt((x6-x5-x4)^2+(y6-y5-y4)^2);
+        System.out.println("Length of the line2 is: "+lengthofline2);
+                if(lengthofline1==lengthofline2){
+            System.out.println("Both the lines are equal");
+        }
+                else {
+            System.out.println("Lines are not equal");
+        }
     }
 }
